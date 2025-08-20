2415edb4d7d0938bf89593abe5b2ab29a3366724
Jenna Lee <65867683+roasted-oolong@users.noreply.github.com>
2025-08-20 19:16:02 +0000

    [Complete] Webpack Practice Lesson from The Odin Project

---
+++ src/index.js
@@ -0,0 +1,11 @@
+// src/index.js
+import "./styles.css";
+import { greeting } from "./greeting.js";
+import odinImage from "./odin.png";
+   
+const image = document.createElement("img");
+image.src = odinImage;
+   
+document.body.appendChild(image);
+
+console.log(greeting);
